a495154c3f992501b5d9087cf5d266449672c9b7
noragami <mkbehforever@gmail.com>
2018-09-04 00:46:39 +0300

    Add switches magic.

--- libs/uix/baseclasses/ico.py
+++ libs/uix/baseclasses/ico.py
@@ -1,8 +1,35 @@
 # -*- coding: utf-8 -*-
 from kivy.uix.screenmanager import Screen
-from kivy.properties import BooleanProperty
+from kivy.properties import ObjectProperty
 
 
 class Ico(Screen):
-    pass
+    icobazaar_ = ObjectProperty()
+    noname_ = ObjectProperty()
+    noname2_ = ObjectProperty()
 
+    def __init__(self, **kwargs):
+        super(Ico, self).__init__(**kwargs)
+        self.cls_inst = None
+
+    def callback(self, instance, value):
+        """Callback method which get instance and value and handle switches magic."""
+        self.icobazaar_.bind(active=self.callback)
+        self.noname_.bind(active=self.callback)
+        self.noname2_.bind(active=self.callback)
+
+        attrs_lst = [self.icobazaar_, self.noname_, self.noname2_]
+
+        # Switches magic.
+        if value is True:
+            for attr in attrs_lst:
+                if attr.name == instance.name:
+                    attrs_lst.remove(attr)
+                    self.cls_inst = instance
+
+            for attr in attrs_lst:
+                attr.active = False
+
+        if value is False:
+            if instance == self.cls_inst:
+                instance.active = True
